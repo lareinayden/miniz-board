5ca4ee6ad1cd9ffe530c038ee5fb2296993ba5fe
Nick Zhang <nickzhang@gatech.edu>
2023-04-19 16:19:16 -0400

    minor updates

--- delay/delay.py
+++ delay/delay.py
@@ -3,8 +3,10 @@ from struct import pack, unpack
 ip = "192.168.0.100"
 port = 2390
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('192.168.0.101',58999))
+#sock.bind(('192.168.0.103',58999))
+sock.bind(('127.0.0.1',58999))
 sent_size = sock.sendto(pack('255s',b'abcd'), (ip, port))
 
-data, addr = sock.recvfrom(1)
+data, addr = sock.recvfrom(64)
+breakpoint()
 print(data)
